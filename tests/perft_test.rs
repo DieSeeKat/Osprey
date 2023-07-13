ae41ac0912f2fea60e9140c2d1fcc66473688596
DieSeeKat <lukasanthonissen@gmail.com>
2023-07-13 12:03:22 +0200

    Add perft tests for starting position

---
+++ tests/perft_test.rs
@@ -0,0 +1,22 @@
+extern crate osprey;
+
+#[cfg(test)]
+mod perft_tests {
+    use crate::osprey::Board;
+
+    #[test]
+    fn starting_position_w() {
+        let board = Board::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        let moves = board.possible_white();
+
+        assert_eq!(moves.len(), 20);
+    }
+
+    #[test]
+    fn starting_position_b() {
+        let board = Board::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1");
+        let moves = board.possible_black();
+
+        assert_eq!(moves.len(), 20);
+    }
+}
